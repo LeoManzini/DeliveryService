1ae4edcac2ed45b242be27169637828af077c6b8
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-05-03 18:09:20 -0300

    Modifying the app class

--- Delivery/src/main/java/br/com/leomanzini/delivery/App.java
+++ Delivery/src/main/java/br/com/leomanzini/delivery/App.java
@@ -1,10 +1,16 @@
 package br.com.leomanzini.delivery;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
+	
+	private static final Logger LOG = LogManager.getLogger(App.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		String propsPath = (args.length > 0) ? args[0] : "src/main/resources/database.properties";
+		
 	}
 
 }
