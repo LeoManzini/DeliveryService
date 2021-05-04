ae3b31a3586be816da4525a4926a93d068c91c1e
Leonardo Henrique Manzini <leoh.manzini@gmail.com>
2021-05-04 12:50:19 -0300

    Adding a method to print each product at each order

--- Delivery/src/main/java/br/com/leomanzini/delivery/App.java
+++ Delivery/src/main/java/br/com/leomanzini/delivery/App.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,15 +22,19 @@ public class App {
 	private static Statement stm;
 	private static ResultSet rs;
 	private static Connection connection;
+	private static Map<Long, Order> mapOrder = new HashMap<>();
+	private static Map<Long, Product> mapProduct = new HashMap<>();
 
-	public static String QUERY = "SELECT * FROM tb_order";
+	public static String QUERY = " SELECT * FROM tb_order "
+							   + " INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id "
+							   + " INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id ";
 	
 	public static Product instantiateProduct(ResultSet rs) {
 
 		Product prod = new Product();
 
 		try {
-			prod.setId(rs.getLong("id"));
+			prod.setId(rs.getLong("product_id"));
 			prod.setName(rs.getString("name"));
 			prod.setPrice(rs.getDouble("price"));
 			prod.setDescription(rs.getString("description"));
@@ -44,7 +50,7 @@ public class App {
 		Order order = new Order();
 
 		try {
-			order.setId(rs.getLong("id"));
+			order.setId(rs.getLong("order_id"));
 			order.setLatitude(rs.getDouble("latitude"));
 			order.setLongitude(rs.getDouble("longitude"));
 			order.setMoment(rs.getTimestamp("moment").toInstant());
@@ -67,12 +73,30 @@ public class App {
 			rs = stm.executeQuery(QUERY);
 
 			while (rs.next()) {
-				//Product product = instantiateProduct(rs);
-				//LOG.info(product);
 				
-				Order order = instantiateOrder(rs);
-				LOG.info(order);
+				Long orderId = rs.getLong("order_id");
+				if(mapOrder.get(orderId) == null) {
+					Order order = instantiateOrder(rs);
+					mapOrder.put(orderId, order);
+				}
+				
+				Long productId = rs.getLong("product_id");
+				if(mapProduct.get(productId) == null) {
+					Product product = instantiateProduct(rs);
+					mapProduct.put(productId, product);
+				}
+				
+				mapOrder.get(orderId).getProducts().add(mapProduct.get(productId));
 			}
+			
+			for(Long orderId : mapOrder.keySet()) {
+				LOG.info(mapOrder.get(orderId));
+				for(Product product : mapOrder.get(orderId).getProducts()) {
+					LOG.info(product);
+				}
+				System.out.println();
+			}
+			
 		} catch (SQLException e) {
 			LOG.error(e.getMessage(), e);
 			System.exit(-1);
